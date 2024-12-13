f64f8950a49cb3aa5069af93dc719a0b191ec177
WW <121015978+clw8998@users.noreply.github.com>
2024-12-13 13:57:34 +0800

    Adjust 64-bit Bootstrap

--- services/bootstrap/src/main.rs
+++ services/bootstrap/src/main.rs
@@ -18,88 +18,20 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[polkavm_derive::polkavm_import]
 extern "C" {
-    #[polkavm_import(index = 0)]
-    pub fn gas() -> i64;
-    #[polkavm_import(index = 1)]
-    pub fn lookup(service: u32, hash_ptr: *const u8, out: *mut u8, out_len: u32) -> u32;
-    #[polkavm_import(index = 2)]
-    pub fn read(service: u32, key_ptr: *const u8, key_len: u32, out: *mut u8, out_len: u32) -> u32;
-
-
-    // #[polkavm_import(index = 3)]
-    // pub fn write(key_ptr: *const u8, key_len: u32, value: *const u8, value_len: u32) -> u32;
-
     #[polkavm_import(index = 3)]
-    pub fn write(ko: u32, kz: u32, bo: u32, bz: u32) -> u32;
-
-
-    #[polkavm_import(index = 4)]
-    pub fn info(service: u32, out: *mut u8) -> u32;
-    #[polkavm_import(index = 5)]
-    pub fn empower(m: u32, a: u32, v: u32, o: u32, n: u32) -> u32;
-    #[polkavm_import(index = 6)]
-    pub fn assign(c: u32, out: *mut u8) -> u32;
-    #[polkavm_import(index = 7)]
-    pub fn designate(out: *mut u8) -> u32;
-    #[polkavm_import(index = 8)]
-    pub fn checkpoint() -> u64;
-
-    // #[polkavm_import(index = 9)]
-    // pub fn new(service: u32, hash_ptr: *const u8, out: *mut u8, out_len: u32) -> u32;
+    pub fn write(ko: u64, kz: u64, bo: u64, bz: u64) -> u64;
 
     #[polkavm_import(index = 9)]
-    pub fn new(o: u32, l: u32, gl: u32, gh: u32, ml: u32, mh: u32) -> u32;
-    
-
-
-    #[polkavm_import(index = 10)]
-    pub fn upgrade(out: *const u8, g: u64, m: u64) -> u32;
-    #[polkavm_import(index = 11)]
-    pub fn transfer(d: u32, a: u64, g: u64, out: *mut u8) -> u32;
-    #[polkavm_import(index = 12)]
-    pub fn quit(d: u32, a: u64, g: u64, out: *mut u8) -> u32;
-
-    // #[polkavm_import(index = 13)]
-    // pub fn solicit(hash_ptr: *const u8, z: u32) -> u32;
-
-    #[polkavm_import(index = 13)]
-    pub fn solicit(o: u32, z: u32) -> u32;
-
-    #[polkavm_import(index = 14)]
-    pub fn forget(hash_ptr: *const u8, z: u32) -> u32;
-    #[polkavm_import(index = 15)]
-    pub fn historical_lookup(service: u32, hash_ptr: *const u8, out: *mut u8, out_len: u32) -> u32;
-    #[polkavm_import(index = 16)]
-    pub fn import(import_index: u32, out: *mut u8, out_len: u32) -> u32;
-    #[polkavm_import(index = 17)]
-    pub fn export(out: *const u8, out_len: u32) -> u32;
-    #[polkavm_import(index = 18)]
-    pub fn machine(out: *const u8, out_len: u32) -> u32;
-    #[polkavm_import(index = 19)]
-    pub fn peek(out: *const u8, out_len: u32, i: u32) -> u32;
-    #[polkavm_import(index = 20)]
-    pub fn poke(n: u32, a: u32, b: u32, l: u32) -> u32;
-    #[polkavm_import(index = 21)]
-    pub fn invoke(n: u32, out: *mut u8) -> u32;
-    #[polkavm_import(index = 22)]
-    pub fn expunge(n: u32) -> u32;
-    #[polkavm_import(index = 99)]
-    pub fn blake2b(data: *const u8, data_len: u32, hash_ptr: *mut u8) -> u32;
-    #[polkavm_import(index = 100)]
-    pub fn blake2s(data: *const u8, data_len: u32, hash_ptr: *mut u8) -> u32;
-    #[polkavm_import(index = 101)]
-    pub fn ecrecover(h: *const u8, v: *const u8, r: *const u8, s: *const u8, out: *mut u8) -> u32;
-    #[polkavm_import(index = 102)]
-    pub fn sha2_256(data: *const u8, data_len: u32, hash_ptr: *mut u8) -> u32;
+    pub fn new(o: u64, l: u64, g: u64, m: u64) -> u64;
 }
 
 #[polkavm_derive::polkavm_export]
-extern "C" fn is_authorized() -> u32 {
+extern "C" fn is_authorized() -> u64 {
     0
 }
 
 #[polkavm_derive::polkavm_export]
-extern "C" fn refine() -> u32 {
+extern "C" fn refine() -> u64 {
     unsafe {
         core::arch::asm!(
             "li a3, 0xFEFF0004",
@@ -110,39 +42,31 @@ extern "C" fn refine() -> u32 {
 }
 
 #[polkavm_derive::polkavm_export]
-extern "C" fn accumulate() -> u32 {
-    let mut omega_7: u32 = 0xFEFF0000;
-    // let omega_8: u32 = 206;
-    let omega_8: u32 = unsafe { *(0xFEFF0020 as *const u32) };
-    let omega_9: u32 = 100;
-    let omega_10: u32 = 0x1000;
-    let omega_11: u32 = 0x3000;
-    let omega_12: u32 = 0x4000;
-
-    let result = unsafe { new(omega_7, omega_8, omega_9, omega_10, omega_11, omega_12) };
-
-    // let solicit_result = unsafe { solicit(omega_7, omega_8) };
-    
+extern "C" fn accumulate() -> u64 {
+    let mut omega_7: u64 = 0xFEFF0000; // CHECK
+    let omega_8: u64 = unsafe { ( *(0xFEFF0020 as *const u64)).into() }; // get code length
+    let omega_9: u64 = 100;  // g -  the minimum gas required in order to execute the Accumulate entry-point of the service's code
+    let omega_10: u64 = 100; // m -  the minimum required for the On Transfer entry-point
+    // new: create service host call
+    let result = unsafe { new(omega_7, omega_8, omega_9, omega_10) };
     unsafe {
-        let ptr1 = 0xFEFDE000 as *mut u32; // 2^32 − 2*ZZ − ZI − P (s) (Writable address)
+        let ptr1 = 0xFEFDE000 as *mut u64; // 2^32 − 2*ZZ − ZI − P (s) (Writable address)
         *ptr1 = 0;
 
-        let ptr2 = 0xFEFDE004 as *mut u32; // 2^32 − 2*ZZ − ZI − P (s) + 4 (Writable address)
+        let ptr2 = 0xFEFDE004 as *mut u64; // 2^32 − 2*ZZ − ZI − P (s) + 4 (Writable address)
         *ptr2 = result;
     }
-    
-    let mut omega_7: u32 = 0xFEFDE000; // 2^32 − 2*ZZ − ZI − P (s) (Writable address)
-    let omega_8: u32 = 4;
-    let omega_9: u32 = 0xFEFDE004; // 2^32 − 2*ZZ − ZI − P (s) + 4 (Writable address)
-    let omega_10: u32 = 4;
-
+    let mut omega_7: u64 = 0xFEFDE000; // 2^32 − 2*ZZ − ZI − P (s) (Writable address), storage key {0,0,0,0}
+    let omega_8: u64 = 4;
+    let omega_9: u64 = 0xFEFDE004; // 2^32 − 2*ZZ − ZI − P (s) + 4 (Writable address), new service index u32
+    let omega_10: u64 = 4;
+    // write: create service host call
     let result = unsafe { write(omega_7, omega_8, omega_9, omega_10) };
-
     omega_7 = result;
     result
 }
 
 #[polkavm_derive::polkavm_export]
-extern "C" fn on_transfer() -> u32 {
+extern "C" fn on_transfer() -> u64 {
     0
 }
