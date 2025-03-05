6ed75311f23947fa2a290b03919310c91db4f1d7
Sourabh Niyogi <sourabh@colorfulnotion.com>
2025-03-05 19:57:06 +0000

    megatron always outputs

--- services/megatron/src/main.rs
+++ services/megatron/src/main.rs
@@ -185,16 +185,6 @@ extern "C" fn accumulate() -> u64 {
     let omega_7 = output_bytes_32.as_ptr() as u64;
     let omega_8 = output_bytes_32.len() as u64;
 
-    if m_n % 2 == 0 {
-        // Write to invalid memory address to obtain an empty hash
-        unsafe {
-            core::arch::asm!(
-                "li a1, 1"
-            );
-        }   
-        return 1;
-    }
-
     unsafe { oyield(omega_7); }
 
     // set the result length to register a1
