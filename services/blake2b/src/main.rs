285c3faaccc2050de5eb2f4b1fcfbc46b649c0fc
WW <121015978+clw8998@users.noreply.github.com>
2025-03-06 16:28:24 +0800

    Refactor: Consolidate repeated functions and constants into utils package

--- services/blake2b/src/main.rs
+++ services/blake2b/src/main.rs
@@ -1,29 +1,12 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
-use simplealloc::SimpleAlloc;
-
-#[global_allocator]
-static ALLOCATOR: SimpleAlloc<4096> = SimpleAlloc::new();
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe {
-        core::arch::asm!("unimp", options(noreturn));
-    }
-}
-
-#[polkavm_derive::polkavm_import]
-extern "C" {
-    #[polkavm_import(index = 3)]
-    pub fn write(ko: u64, kz: u64, bo: u64, bz: u64) -> u64;
-}
-
-pub const NONE: u64 = u64::MAX;
-
 use core::convert::TryInto;
+
+use utils::{NONE};
+use utils::{parse_refine_args, parse_wrangled_operand_tuple};
+use utils::{write};
+use utils::{call_info};
 
 const IV: [u64; 8] = [
     0x6A09_E667_F3BC_C908,
@@ -193,19 +176,40 @@ fn blake2b_hash(data: &[u8]) -> [u8; Blake2b::DIGEST_LENGTH] {
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn refine() -> u64 {
-    let omega_7: u64;
+    // read the input start address and length from register a0 and a1
+    let omega_7: u64; // refine input start address
+    let omega_8: u64; // refine input length
+
     unsafe {
         core::arch::asm!(
             "mv {0}, a0",
+            "mv {1}, a1",
             out(reg) omega_7,
+            out(reg) omega_8,
         );
     }
 
-    let input_address: u64 = omega_7 + 4 + 4; // skip time service index and input length
-    let input_length_address: u64 = omega_7 + 4; // skip service index
+    // parse refine args
+    let (wi_service_index, wi_payload_start_address, wi_payload_length, _wphash) =
+    if let Some(args) = parse_refine_args(omega_7, omega_8)
+    {
+        (
+            args.wi_service_index,
+            args.wi_payload_start_address,
+            args.wi_payload_length,
+            args.wphash,
+        )
+    } else {
+        call_info("parse refine args failed");
+        return NONE;
+    };
+
+    call_info("parse refine args success");
+
+    let input_length_address: u64 = wi_payload_start_address;
     let input_length: u64  = unsafe { ( *(input_length_address  as *const u32)).into() }; // skip service index
 
-
+    let input_address: u64 = wi_payload_start_address + 4;
     let input = unsafe { core::slice::from_raw_parts(input_address as *const u8, input_length as usize) };
     let output = blake2b_hash(input);
     
@@ -234,84 +238,21 @@ extern "C" fn accumulate() -> u64 {
             out(reg) omega_8,
         );
     }
+    // fetch service index
+    let service_index_address = omega_7 + 4; // skip 4 bytes time slot
+    let SERVICE_INDEX: u64   = unsafe { ( *(service_index_address as *const u32)).into() }; // 4 bytes service index
 
-    // fetch all_accumulation_o 
+    // fetch all_accumulation_o
     let mut start_address = omega_7 + 4 + 4; // 4 bytes time slot + 4 bytes service index
     let mut remaining_length = omega_8 - 4 - 4; // 4 bytes time slot + 4 bytes service index
-    let all_accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-
-    // fetch the number of accumulation_o
-    let all_accumulation_o_discriminator_length = extract_discriminator(all_accumulation_o);
-    let num_of_accumulation_o = decode_e(&all_accumulation_o[..all_accumulation_o_discriminator_length as usize]);
-
-    // update the address pointer and remaining length
-    start_address += all_accumulation_o_discriminator_length as u64;
-    remaining_length -= all_accumulation_o_discriminator_length as u64;
-
-    // set variables for storing work result address and length
-    let mut work_result_address: u64 = 0;
-    let mut work_result_length: u64 = 0;
-
-    // set variables for storing auth output address and length
-    let mut auth_output_address: u64 = 0;
-    let mut auth_output_length: u64 = 0;
     
-    for n in 0.. num_of_accumulation_o {
-        // we only use the 0th accumulation_o
-        if n > 0 {
-            break;
-        }
-        // fetch work result prefix
-        let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-        let work_result_prefix = &accumulation_o[..1];
-
-        start_address += 1;
-        remaining_length -= 1;
-        
-        // fetch work result
-        if work_result_prefix[0] == 0 {  
-            let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-            let work_result_discriminator_length = extract_discriminator(accumulation_o);
-            work_result_length = if work_result_discriminator_length > 0 {
-                decode_e(&accumulation_o[..work_result_discriminator_length as usize])
-            } else {
-                0
-            };
-            
-            start_address += work_result_discriminator_length as u64;
-            remaining_length -= work_result_discriminator_length as u64;
-            
-            // store the work result address
-            work_result_address = start_address;
-            
-            // update the address pointer and remaining length
-            start_address += work_result_length as u64;
-            remaining_length -= work_result_length as u64;
-        }
-
-        // skip l, k which are two 32 bytes hashes
-        start_address += 32 + 32;
-        remaining_length -= 32 + 32;
-
-        // fetch auth output prefix
-        let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-        let auth_output_discriminator_length = extract_discriminator(accumulation_o);
-        auth_output_length = if auth_output_discriminator_length > 0 {
-            decode_e(&accumulation_o[..auth_output_discriminator_length as usize])
-        } else {
-            0
-        };
-
-        start_address += auth_output_discriminator_length as u64;
-        remaining_length -= auth_output_discriminator_length as u64;
-
-        // store the auth output address
-        auth_output_address = start_address;
-
-        // update the address pointer and remaining length
-        start_address += auth_output_length as u64;
-        remaining_length -= auth_output_length as u64;
-    }
+    let (work_result_address, work_result_length) =
+    if let Some(tuple) = parse_wrangled_operand_tuple(start_address, remaining_length, 0)
+    {
+        (tuple.work_result_ptr, tuple.work_result_len)
+    } else {
+        return NONE;
+    };
 
     // write FIB result to storage
     let key = [0u8; 1];
@@ -332,59 +273,5 @@ extern "C" fn accumulate() -> u64 {
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn on_transfer() -> u64 {
-    0
-}
-
-// some helpful functions
-fn extract_discriminator(input: &[u8]) -> u8 {
-    if input.is_empty() {
-        return 0;
-    }
-
-    let first_byte = input[0];
-    match first_byte {
-        1..=127 => 1,
-        128..=191 => 2,
-        192..=223 => 3,
-        224..=239 => 4,
-        240..=247 => 5,
-        248..=251 => 6,
-        252..=253 => 7,
-        254..=u8::MAX => 8,
-        _ => 0,
-    }
-}
-
-fn power_of_two(exp: u32) -> u64 {
-    1 << exp
-}
-
-fn decode_e_l(encoded: &[u8]) -> u64 {
-    let mut x: u64 = 0;
-    for &byte in encoded.iter().rev() {
-        x = x.wrapping_mul(256).wrapping_add(byte as u64);
-    }
-    x
-}
-
-fn decode_e(encoded: &[u8]) -> u64 {
-    let first_byte = encoded[0];
-    if first_byte == 0 {
-        return 0;
-    }
-    if first_byte == 255 {
-        return decode_e_l(&encoded[1..9]);
-    }
-    for l in 0..8 {
-        let left_bound  = 256 - power_of_two(8 - l);
-        let right_bound = 256 - power_of_two(8 - (l + 1));
-
-        if (first_byte as u64) >= left_bound && (first_byte as u64) < right_bound {
-            let x1 = (first_byte as u64) - left_bound;
-            let x2 = decode_e_l(&encoded[1..(1 + l as usize)]);
-            let x = x1 * power_of_two(8 * l) + x2;
-            return x;
-        }
-    }
     0
 }
--- services/transfer/src/main.rs
+++ services/transfer/src/main.rs
@@ -1,49 +1,52 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
+use utils::{NONE, OK, PAGE_SIZE, SEGMENT_SIZE};
+use utils::{parse_refine_args, parse_wrangled_operand_tuple};
+use utils::{call_info, setup_page, get_page, serialize_gas_and_registers, deserialize_gas_and_registers};
 
-use alloc::vec::Vec;
-use simplealloc::SimpleAlloc;
-
-#[global_allocator]
-static ALLOCATOR: SimpleAlloc<4096> = SimpleAlloc::new();
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe {
-        core::arch::asm!("unimp", options(noreturn));
-    }
-}
-
-#[polkavm_derive::polkavm_import]
-extern "C" {
-    #[polkavm_import(index = 11)]
-    pub fn transfer(d: u64, a: u64, g: u64, out: u64) -> u64;
-}
+use utils::{transfer};
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn refine() -> u64 {
-    // get the input start address form register a0
+    // read the input start address and length from register a0 and a1
     let omega_7: u64; // refine input start address
+    let omega_8: u64; // refine input length
+
     unsafe {
         core::arch::asm!(
             "mv {0}, a0",
+            "mv {1}, a1",
             out(reg) omega_7,
+            out(reg) omega_8,
         );
     }
 
-    let output_len: u64 = 8; // 4 bytes service index + 4 bytes amount
+    // parse refine args
+    let (wi_service_index, wi_payload_start_address, wi_payload_length, _wphash) =
+    if let Some(args) = parse_refine_args(omega_7, omega_8)
+    {
+        (
+            args.wi_service_index,
+            args.wi_payload_start_address,
+            args.wi_payload_length,
+            args.wphash,
+        )
+    } else {
+        call_info("parse refine args failed");
+        return NONE;
+    };
+
+    call_info("parse refine args success");
 
     // set the output address to register a0 and output length to register a1
     unsafe {
         core::arch::asm!(
             "mv a1, {0}",
-            in(reg) output_len,
+            in(reg) wi_payload_length,
         );
     }
-    // this equals to a0 = omega_7 + 4
-    omega_7 + 4 // eliminate the first 4 bytes (workitem service index)
+    wi_payload_start_address
 }
 
 #[polkavm_derive::polkavm_export]
@@ -60,84 +63,21 @@ extern "C" fn accumulate() -> u64 {
             out(reg) omega_8,
         );
     }
+    // fetch service index
+    let service_index_address = omega_7 + 4; // skip 4 bytes time slot
+    let SERVICE_INDEX: u64   = unsafe { ( *(service_index_address as *const u32)).into() }; // 4 bytes service index
 
-    // fetch all_accumulation_o 
+    // fetch all_accumulation_o
     let mut start_address = omega_7 + 4 + 4; // 4 bytes time slot + 4 bytes service index
     let mut remaining_length = omega_8 - 4 - 4; // 4 bytes time slot + 4 bytes service index
-    let all_accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-
-    // fetch the number of accumulation_o
-    let all_accumulation_o_discriminator_length = extract_discriminator(all_accumulation_o);
-    let num_of_accumulation_o = decode_e(&all_accumulation_o[..all_accumulation_o_discriminator_length as usize]);
-
-    // update the address pointer and remaining length
-    start_address += all_accumulation_o_discriminator_length as u64;
-    remaining_length -= all_accumulation_o_discriminator_length as u64;
-
-    // set variables for storing work result address and length
-    let mut work_result_address: u64 = 0;
-    let mut work_result_length: u64 = 0;
-
-    // set variables for storing auth output address and length
-    let mut auth_output_address: u64 = 0;
-    let mut auth_output_length: u64 = 0;
     
-    for n in 0.. num_of_accumulation_o {
-        // we only use the 0th accumulation_o
-        if n > 0 {
-            break;
-        }
-        // fetch work result prefix
-        let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-        let work_result_prefix = &accumulation_o[..1];
-
-        start_address += 1;
-        remaining_length -= 1;
-        
-        // fetch work result
-        if work_result_prefix[0] == 0 {  
-            let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-            let work_result_discriminator_length = extract_discriminator(accumulation_o);
-            work_result_length = if work_result_discriminator_length > 0 {
-                decode_e(&accumulation_o[..work_result_discriminator_length as usize])
-            } else {
-                0
-            };
-            
-            start_address += work_result_discriminator_length as u64;
-            remaining_length -= work_result_discriminator_length as u64;
-            
-            // store the work result address
-            work_result_address = start_address;
-            
-            // update the address pointer and remaining length
-            start_address += work_result_length as u64;
-            remaining_length -= work_result_length as u64;
-        }
-
-        // skip l, k which are two 32 bytes hashes
-        start_address += 32 + 32;
-        remaining_length -= 32 + 32;
-
-        // fetch auth output prefix
-        let accumulation_o = unsafe { core::slice::from_raw_parts(start_address as *const u8, remaining_length as usize) };
-        let auth_output_discriminator_length = extract_discriminator(accumulation_o);
-        auth_output_length = if auth_output_discriminator_length > 0 {
-            decode_e(&accumulation_o[..auth_output_discriminator_length as usize])
-        } else {
-            0
-        };
-
-        start_address += auth_output_discriminator_length as u64;
-        remaining_length -= auth_output_discriminator_length as u64;
-
-        // store the auth output address
-        auth_output_address = start_address;
-
-        // update the address pointer and remaining length
-        start_address += auth_output_length as u64;
-        remaining_length -= auth_output_length as u64;
-    }
+    let (work_result_address, work_result_length) =
+    if let Some(tuple) = parse_wrangled_operand_tuple(start_address, remaining_length, 0)
+    {
+        (tuple.work_result_ptr, tuple.work_result_len)
+    } else {
+        return NONE;
+    };
 
     // get amount address
     let amount_address = work_result_address + work_result_length - 4;
@@ -171,59 +111,5 @@ extern "C" fn on_transfer() -> u64 {
     // // balance to storage
     // let result = unsafe { write(ko, kz, vo, vz) };
     // result
-    0
-}
-
-// some helpful functions
-fn extract_discriminator(input: &[u8]) -> u8 {
-    if input.is_empty() {
-        return 0;
-    }
-
-    let first_byte = input[0];
-    match first_byte {
-        1..=127 => 1,
-        128..=191 => 2,
-        192..=223 => 3,
-        224..=239 => 4,
-        240..=247 => 5,
-        248..=251 => 6,
-        252..=253 => 7,
-        254..=u8::MAX => 8,
-        _ => 0,
-    }
-}
-
-fn power_of_two(exp: u32) -> u64 {
-    1 << exp
-}
-
-fn decode_e_l(encoded: &[u8]) -> u64 {
-    let mut x: u64 = 0;
-    for &byte in encoded.iter().rev() {
-        x = x.wrapping_mul(256).wrapping_add(byte as u64);
-    }
-    x
-}
-
-fn decode_e(encoded: &[u8]) -> u64 {
-    let first_byte = encoded[0];
-    if first_byte == 0 {
-        return 0;
-    }
-    if first_byte == 255 {
-        return decode_e_l(&encoded[1..9]);
-    }
-    for l in 0..8 {
-        let left_bound  = 256 - power_of_two(8 - l);
-        let right_bound = 256 - power_of_two(8 - (l + 1));
-
-        if (first_byte as u64) >= left_bound && (first_byte as u64) < right_bound {
-            let x1 = (first_byte as u64) - left_bound;
-            let x2 = decode_e_l(&encoded[1..(1 + l as usize)]);
-            let x = x1 * power_of_two(8 * l) + x2;
-            return x;
-        }
-    }
     0
 }
