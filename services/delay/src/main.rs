754e593f53571d1b9ac0a322cf9a712eda3bf4b2
WW <121015978+clw8998@users.noreply.github.com>
2025-03-06 16:28:24 +0800

    Refactor: Consolidate repeated functions and constants into utils package

--- services/delay/src/main.rs
+++ services/delay/src/main.rs
@@ -1,27 +1,13 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
-use simplealloc::SimpleAlloc;
-
-#[global_allocator]
-static ALLOCATOR: SimpleAlloc<4096> = SimpleAlloc::new();
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe {
-        core::arch::asm!("unimp", options(noreturn));
-    }
-}
+use utils::{NONE};
 
 #[polkavm_derive::polkavm_import]
 extern "C" {
     #[polkavm_import(index = 99)]
     pub fn delay(seconds: u64) -> u64;
 }
-
-pub const NONE: u64 = u64::MAX;
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn refine() -> u32 {
