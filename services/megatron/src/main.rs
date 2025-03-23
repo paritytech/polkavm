59a5cc2c8bd2db7db366779aab2a7f238ec55cea
Sourabh Niyogi <sourabhniyogi@users.noreply.github.com>
2025-03-23 03:11:46 -0700

    add fib2 exports logging + add megatron transfers (#7)
    adjustments:
    * corevm/fib2 to have systemic logging
    * megatron to have variable number of transfers and include logging
    * null_authorizer to do gas + call_log 3x times

--- services/megatron/src/main.rs
+++ services/megatron/src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 
 use utils::constants::FIRST_READABLE_ADDRESS;
-use utils::functions::{parse_accumulate_args, parse_refine_args};
-use utils::host_functions::{oyield, read, write};
+use utils::functions::{parse_accumulate_args, parse_refine_args, call_log};
+use utils::host_functions::{oyield, read, write, transfer};
+
+extern crate alloc;
+use alloc::format;
 
 #[polkavm_derive::polkavm_export]
 extern "C" fn refine(start_address: u64, length: u64) -> (u64, u64) {
@@ -74,17 +77,42 @@ extern "C" fn accumulate(start_address: u64, length: u64) -> (u64, u64) {
     let s1_vn = u32::from_le_bytes(buffer1[4..8].try_into().unwrap());
     let s1_vnminus1 = u32::from_le_bytes(buffer1[8..12].try_into().unwrap());
 
-    // calculate the new values and write to storage
     let m_n = s0_n;
+
+    // calculate the new values and write to storage
+    call_log(2, None, &format!("meg {:?} read from service {:?} fib(n={:?})={:?}", m_n, service0, s0_n, s0_vn));
+    call_log(2, None, &format!("meg {:?} read from service {:?} trib(n={:?})={:?}", m_n, service1, s1_vn, s1_vn));
     let m_vn = s0_vn + s1_vn;
     let m_vnminus1 = s0_vnminus1 + s1_vnminus1;
-
+    let memo = [0u8; 128];
+    let send0 = (m_n % 3 == 0) || (m_n % 3 == 2);
+    let send1 = (m_n % 3 == 1) || (m_n % 3 == 2);
+    let omega_10: u64 = memo.as_ptr() as u64; // memo
+    let amount0: u64 = m_n as u64;
+    let amount1: u64 = (m_n * 2 + 1) as u64;
+    let gasAvail: u64 = 100;
+    let mut numTransfers = 0;
+    for i in 0..m_n {
+        if send0 {
+            let result0 = unsafe { transfer(service0, amount0, gasAvail, omega_10) };
+            call_log(2, None, &format!("{:?} transfer(dest:{:?}, amount={:?}, gasAvail={:?}) Result: {:?}", i, service0, amount0, gasAvail, result0));
+            numTransfers = numTransfers + 1;
+        }
+        if send1 {
+            let result1 = unsafe { transfer(service0, amount1, gasAvail, omega_10) };
+            call_log(2, None, &format!("{:?} transfer(dest:{:?}, amount={:?}, gasAvail={:?}) Result: {:?}", i, service1, amount1, gasAvail, result1));
+            numTransfers = numTransfers + 1;
+        }
+    }
+     
     buffer[0..4].copy_from_slice(&m_n.to_le_bytes());
     buffer[4..8].copy_from_slice(&m_vn.to_le_bytes());
     buffer[8..12].copy_from_slice(&m_vnminus1.to_le_bytes());
+    call_log(2, None, &format!("meg({:?})={:?}", m_n, m_vn));
     unsafe {
         write(key.as_ptr() as u64, key.len() as u64, buffer.as_ptr() as u64, buffer.len() as u64);
     }
+    call_log(2, None, &format!("meg {:?} write(n={:?}) numTransfers={:?}", m_n, m_vn, numTransfers));
 
     // Option<hash> test
     // pad result to 32 bytes
--- services/utils/src/constants.rs
+++ services/utils/src/constants.rs
@@ -1,6 +1,13 @@
 pub const NONE: u64 = u64::MAX;
 pub const OOB: u64 = u64::MAX - 2;
 pub const OK: u64 = 0;
+pub const WHAT: u64 = u64::MAX - 1;       // (1 << 64) - 2
+pub const WHO: u64 = u64::MAX - 3;        // (1 << 64) - 4
+pub const FULL: u64 = u64::MAX - 4;       // (1 << 64) - 5
+pub const CORE: u64 = u64::MAX - 5;       // (1 << 64) - 6
+pub const CASH: u64 = u64::MAX - 6;       // (1 << 64) - 7
+pub const LOW: u64 = u64::MAX - 7;        // (1 << 64) - 8
+pub const HUH: u64 = u64::MAX - 8;        // (1 << 64) - 9
 
 pub const SEGMENT_SIZE: u64 = 4104;
 pub const PARENT_MACHINE_INDEX: u64 = (1u64 << 32) - 1;
