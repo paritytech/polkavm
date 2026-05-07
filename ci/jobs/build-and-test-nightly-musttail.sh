b015c11d5fd9b847a5b23d4705e9729f73dd2895
kvp <223931578+kvpanch@users.noreply.github.com>
2026-05-06 22:12:12 -0400

    enable unprivileged_userfaultfd

--- ci/jobs/build-and-test-nightly-musttail.sh
+++ ci/jobs/build-and-test-nightly-musttail.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 cd ../..
 
+if [ "$(cat /proc/sys/vm/unprivileged_userfaultfd)" != "1" ]; then
+    echo "1" | sudo tee /proc/sys/vm/unprivileged_userfaultfd > /dev/null
+fi
+
 echo ">> cargo +nightly build (polkavm, experimental-musttail)"
 cargo +nightly build -p polkavm --features experimental-musttail
 
