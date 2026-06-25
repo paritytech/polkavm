1e048740bb54ab0435afae1d522950c894c3fc19
Jan Bujak <jan@parity.io>
2026-06-25 08:14:51 +0000

    Publicly export `CorePinning`

--- crates/polkavm/src/lib.rs
+++ crates/polkavm/src/lib.rs
@@ -149,7 +149,7 @@ pub mod program {
 pub type Gas = i64;
 
 pub use crate::api::{CompileError, Engine, MemoryAccessError, MemoryProtection, Module, RawInstance, RegValue, SetCacheSizeLimitArgs};
-pub use crate::config::{BackendKind, Config, CustomCodegen, GasMeteringKind, ModuleConfig, SandboxKind};
+pub use crate::config::{BackendKind, Config, CorePinning, CustomCodegen, GasMeteringKind, ModuleConfig, SandboxKind};
 pub use crate::error::Error;
 pub use crate::gas::{Cost, CostModel, CostModelKind, CostModelRef};
 pub use crate::linker::{CallError, Caller, Instance, InstancePre, Linker};
